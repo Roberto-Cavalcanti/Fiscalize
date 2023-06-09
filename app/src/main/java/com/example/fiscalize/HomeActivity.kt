7a78bed24b04fdc0888f3711896c0ce0f75af17b
Roberto-Cavalcanti <rickfreitas2001@gmail.com>
2023-06-08 23:21:07 -0300

    segunda Tela

--- app/src/main/java/com/example/fiscalize/HomeActivity.kt
+++ app/src/main/java/com/example/fiscalize/HomeActivity.kt
@@ -7,5 +7,12 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        greeting()
     }
+
+    private fun greeting(){
+        val nome = intent.getStringExtra("nome")
+        
+    }
+
 }
